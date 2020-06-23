d37de3e91ccb1bd1aa82568f0a57b4ba12581e64
Gareth Edwards <garrethedwards@gmail.com>
2020-06-23 13:20:01 +0200

    Refactor makefiles submodule

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sdc-global-makeFile
-The global sdc MakeFile to be used within all repos
+# sdc-global-makeFiles
+
+A collection of makefiles for all sdc repos. This is to allow make commands to be re-usable and be able to change on place.
+
+## How to use
+
+1. add a submodule in the makefiles dir. This will add this repo inside your project so the included makefiles can be used
+2. Create a Makefile in the route of your repo
+3. Ensure the Makefile includes the new `.mk` files like this `include makefiles/*.mk` or individual makefiles like `include makefiles/test.mk` etc.
+4. you can then use make commands like before `make test` 
--- test.mk
+++ test.mk
@@ -4,10 +4,6 @@ MYSQL_CREDS := $(if $(MYSQL_ALLOW_EMPTY_PASSWORD),-uroot,-uroot -p)
 MYSQL_DATABASE ?= sdc
 COVERAGE_THRESHOLD ?= 90
 REPO_NAME ?= ''
-UNAME := $(shell uname -m)
-CURRENT_DIR= $(shell pwd)
-AWS_CLI_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
-MESSAGE ?= 'none'
 
 build:
 	make install-requirements;
@@ -55,36 +51,3 @@ test-dependant-no-db:
 	sed -i 's#${PACKAGE_NAME}==${VERSION}#git+https://lucidlogic:${ACCESS_TOKEN}@github.com/${REPO_NAME}/@${BRANCH_NAME}#g' requirements.txt
 	make install-requirements;
 	make test;
-
-update-submodule:
-	git submodule update --remote --merge
-
-install-aws-cli:
-	curl $(AWS_CLI_URL) -o "awscliv2.zip"
-	unzip awscliv2.zip
-	sudo ./aws/install
-	aws --version
-
-update-lambda:
-	aws lambda update-function-code --function-name  ${FUNCTION_NAME} --zip-file fileb://lambda.zip
-
-invoke:
-	aws lambda invoke --function-name  ${FUNCTION_NAME} --payload ${PAYLOAD} ./response.json
-
-check-response:
-	$(eval ERROR := $(shell jq 'select(.errorMessage != null) | .errorMessage' ./response.json))
-
-	if [ ${ERROR} ]; then\
-		echo ${ERROR};exit 1; \
-	fi
-
-publish-lambda:
-	$(eval VERSION := $(shell aws lambda publish-version --function-name  ${FUNCTION_NAME} --description "${MESSAGE:0:250}" --query 'Version' --output text))
-	aws lambda update-alias --function-name  ${FUNCTION_NAME} --name master --function-version ${VERSION}
-
-update-version:
-	$(eval VERSION := $(shell grep version= setup.py | grep -Eo '[0-9]+([.][0-9]+)([.][0-9]+)?'))
-	sed -i 's#${VERSION}#$(REF:refs/tags/v%=%)#g' setup.py
-
-init:
-	ln -s make/Makefile ../Makefile
