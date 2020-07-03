c44d8297ba31859f69e6127b57759ad8596699d5
Gareth Edwards <garrethedwards@gmail.com>
2020-07-03 13:04:05 +0200

    Add auto lint make command

--- base.mk
+++ base.mk
@@ -10,3 +10,9 @@ install-local-requirements:
 
 update-submodule:
 	git submodule update --remote --merge
+
+auto-lint:
+	pip install --upgrade autopep8
+	mkdir .git/hooks
+	curl https://gist.githubusercontent.com/lucidlogic/ef3de91857197512944bcde82c5fdb03/raw/7d3e880e53f68935b50093fc0c9cfebf7a6669f7/pre-commit --output .git/hooks/pre-commit
+	chmod +x .git/hooks/pre-commit
